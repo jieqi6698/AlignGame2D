ea2c10331c0b25a83fc3a29cd1d094d3232c31a2
YongJia Chen <chenyoca@gmail.com>
2012-05-19 18:09:08 +0800

    Splash scene finish !

--- Classes/ui/ag2d_splashscene.cpp
+++ Classes/ui/ag2d_splashscene.cpp
@@ -2,45 +2,56 @@
 
 using namespace ag2d;
 
-Ag2dSplashScene::Ag2dSplashScene(FlowCode code)
+Ag2dSplashScene::Ag2dSplashScene(FlowCode code,char* splash)
 	:Ag2dCommonScene(code),
-	m_splash_list(NULL),
-	m_splash_index(0)
+	m_splash_image(splash),
+	m_listener(NULL),
+	m_loading_icon(NULL)
 {
-	m_splash_list = new list<char*>;
 }
 
-void Ag2dSplashScene::setSplashRes(char* splash)
+void Ag2dSplashScene::setLoadingListener(Ag2dSplashListener& listener)
 {
-	m_splash_list->push_back(splash);
-	m_splash_index++;
+	m_listener = &listener;
+}
+
+void Ag2dSplashScene::setLoadingIcon(char* load_icon)
+{
+	m_loading_icon = load_icon;
 }
 
 void Ag2dSplashScene::updateBackgroundLayer(CCLayer& bg_layer)
 {
-		
 }
 
 void Ag2dSplashScene::updateWindowsLayer(CCLayer& wnd_layer)
 {
-		
-}
-		
-void Ag2dSplashScene::drawToBackgroundLayer(CCLayer& bg_layer)
-{
-	CCSprite* bg = CCSprite::spriteWithFile("background.jpg");
-	bg->setPosition(ccp(m_center_x,m_center_y));
-	bg_layer.addChild(bg);
-
-	this->schedule(schedule_selector(Ag2dSplashScene::splashCallback),2.5f);
-}
-
-void Ag2dSplashScene::splashCallback(ccTime timc)
-{
-	CCLOG("Splash Callback");
 }
 
 void Ag2dSplashScene::drawToWindowsLayer(CCLayer& wnd_layer)
 {
+}
 		
+void Ag2dSplashScene::drawToBackgroundLayer(CCLayer& bg_layer)
+{
+	CCSprite* bg = CCSprite::spriteWithFile(m_splash_image);
+	bg->setPosition(ccp(m_center_x,m_center_y));
+	bg_layer.addChild(bg);
+	bg->runAction(CCFadeIn::actionWithDuration(0.5f));
+	
+	if(NULL != m_loading_icon)
+	{
+		CCSprite* loading_icon = CCSprite::spriteWithFile(m_loading_icon);
+		int padding = 5;
+		float x = m_measure_width - loading_icon->getContentSize().width/2 - padding;
+		float y = loading_icon->getContentSize().height - padding;
+		loading_icon->setPosition(ccp(x,y));
+		loading_icon->runAction(CCRepeatForever::actionWithAction(CCRotateBy::actionWithDuration(3 , 360)));
+		bg_layer.addChild(loading_icon);
+	}
+	//启动加载过程
+	if( NULL!= m_listener) m_listener->onLoading();
+
+	//中转到下一个场景
+
 }
